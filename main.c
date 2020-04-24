fb6f08f0248799dc91a2d96b3523fed28b97a6a9
pfijal <61974066+pfijal@users.noreply.github.com>
2020-04-25 01:17:38 +0200

    Nastąpiła pomyłka.
    Kilka dni temu dodałem raport i projekt do sklonowanego repozytorium, zamiast dodać je tutaj.

--- main.c
+++ main.c
@@ -3,63 +3,212 @@
 #include <stdlib.h>
 #include <time.h>
 
-unsigned int ns[] = { 10, /* TODO: fill in "n" i.e. instance sizes */ };
+unsigned int ns[] = { 10,100,1000 ,10000, 100000};
 
-void fill_increasing(int *t, unsigned int n) {
-    // TODO: implement
+void fill_increasing(int *t, unsigned int n)
+{
+    for(int i = 0; i < n; i++)
+    {
+        t[i] = i;
+    }
 }
 
-void fill_decreasing(int *t, unsigned int n) {
-    // TODO: implement
+void fill_decreasing(int *t, unsigned int n)
+{
+    for (int i = 0 ; i < n ; i++ )
+    {
+        t[i] = n-i;
+
+    }
+}
+void fill_vshape(int *t, unsigned int n)
+{
+    int i = 0, j = 0;
+    int m = n-1;
+
+
+
+
+    for (int i = 0; i <= (n/2); i++)
+    {
+        //JUMP from assigning n-i to t[i] or t[n-1]
+        // i = 1, t[0] = n
+
+        t[i] = m-j;
+        t[m-i] = t[i]-1;
+        j++;
+        j++;
+
+
+
+    }
 }
 
-void fill_vshape(int *t, unsigned int n) {
-    // TODO: implement
+//POCZATEK SELECTION SORT
+void selection_sort (int *t, unsigned n)
+{
+    int temp;
+    int j;
+    int i;
+    for (i = 0; i < n; i++)
+    {
+
+        j = argmin(t, n, i);
+
+        temp = t[j];
+        t[j] = t[i];
+        t[i] = temp;
+        ;
+
+
+    }
 }
 
-void selection_sort(int *t, unsigned int n) {
-    // TODO: implement
+int argmin(int *t, unsigned n, int i)
+{
+    int argmin = i;
+    for (int j = i+1; j < n; j++)
+    {
+        if(t[j] < t[argmin])
+        {
+            argmin = j;
+        }
+    }
+
+    return argmin;
+}
+//KONIEC SELECTION SORT
+
+
+
+void insertion_sort(int *t, unsigned int n)
+{
+
+    int i,j,key;
+
+    for(j = 1; j < n; j++)
+    {
+
+        key = t[j];
+        i = j-1;
+
+        while (i>=0 && t[i]>key)
+        {
+            t[i+1] = t[i];
+            i--;
+        }
+        t[i+1] = key;
+    }
+
 }
 
-void insertion_sort(int *t, unsigned int n) {
-    // TODO: implement
+
+
+//POCZATEK QUICK_SORT
+void quicksort(int *t, int p, int r);
+
+
+int partition(int *t, int p, int r)
+{
+    int piv,i,j,tmp;
+    piv = t[r];
+    i = p - 1;
+    for (j = p ; j < r; j++)
+    {
+        if( t[j] <= piv )
+        {
+            i++;
+            tmp = t[i];
+            t[i] = t[j];
+            t[j] = tmp;
+
+        }
+    }
+
+    i++;
+    tmp = t[i];
+    t[i] = t[r];
+    t[r] = tmp;
+    return i;
 }
 
-void quick_sort(int *t, unsigned int n) {
-    // TODO: implement
+int quick_random(int *t, int p, int r)
+{
+
+
+
+    int i = p + rand() % (r-p);
+    int tmp;
+
+    tmp = t[i];
+    t[i] = t[r];
+    t[r] = tmp;
+
+    return partition(t,p,r);
+
 }
 
-void heap_sort(int *t, unsigned int n) {
-    // TODO
+void quicksort(int *t, int p, int r)
+{
+    int q;
+    if (p<r)
+    {
+        q = quick_random(t, p, r);
+        //q = partition(t, p, r);
+        quicksort(t, q+1, r);
+        quicksort(t, p, q-1);
+    }
 }
 
-void fill_random(int *t, unsigned int n) {
-    for (unsigned int i = 0; i < n; i++) {
+void quick_sort(int *t, unsigned int n)
+{
+
+    quicksort(t, 0, n-1);
+}
+//KONIEC QUICK_SORT
+
+
+void heap_sort(int *t, unsigned int n)
+{
+    ;// TODO
+}
+
+void fill_random(int *t, unsigned int n)
+{
+    for (unsigned int i = 0; i < n; i++)
+    {
         t[i] = rand();
     }
 }
 
-void is_random(int *t, unsigned int n) {
+void is_random(int *t, unsigned int n)
+{
     return;
 }
 
-void is_increasing(int *t, unsigned int n) {
-    for (unsigned int i = 1; i < n; i++) {
+void is_increasing(int *t, unsigned int n)
+{
+    for (unsigned int i = 1; i < n; i++)
+    {
         assert(t[i] > t[i - 1]);
     }
 }
 
-void is_decreasing(int *t, unsigned int n) {
-    for (unsigned int i = 1; i < n; i++) {
+void is_decreasing(int *t, unsigned int n)
+{
+    for (unsigned int i = 1; i < n; i++)
+    {
         assert(t[i] < t[i - 1]);
     }
 }
 
-void is_vshape(int *t, unsigned int n) {
+void is_vshape(int *t, unsigned int n)
+{
     int *begin = t;
     int *end = t + n - 1;
 
-    while (end - begin > 1) {
+    while (end - begin > 1)
+    {
         assert(*begin > *end);
         begin++;
         assert(*end > *begin);
@@ -67,28 +216,34 @@ void is_vshape(int *t, unsigned int n) {
     }
 }
 
-void is_sorted(int *t, unsigned int n) {
-    for (unsigned int i = 1; i < n; i++) {
+void is_sorted(int *t, unsigned int n)
+{
+    for (unsigned int i = 1; i < n; i++)
+    {
         assert(t[i] >= t[i - 1]);
     }
 }
 
 void (*fill_functions[])(int *, unsigned int) = { fill_random, fill_increasing, fill_decreasing, fill_vshape };
 void (*check_functions[])(int *, unsigned int) = { is_random, is_increasing, is_decreasing, is_vshape };
-void (*sort_functions[])(int *, unsigned int) = { selection_sort, insertion_sort, quick_sort, heap_sort };
+void (*sort_functions[])(int *, unsigned int) = { selection_sort, insertion_sort, quick_sort, /* heap_sort */};
 
 char *fill_names[] = { "Random", "Increasing", "Decreasing", "V-Shape" };
 char *sort_names[] = { "SelectionSort", "InsertionSort", "QuickSort", "HeapSort" };
 
-int main() {
-    for (unsigned int i = 0; i < sizeof(sort_functions) / sizeof(*sort_functions); i++) {
+int main()
+{
+    for (unsigned int i = 0; i < sizeof(sort_functions) / sizeof(*sort_functions); i++)
+    {
         void (*sort)(int *, unsigned int) = sort_functions[i];
 
-        for (unsigned int j = 0; j < sizeof(fill_functions) / sizeof(*fill_functions); j++) {
+        for (unsigned int j = 0; j < sizeof(fill_functions) / sizeof(*fill_functions); j++)
+        {
             void (*fill)(int *, unsigned int) = fill_functions[j];
             void (*check)(int *, unsigned int) = check_functions[j];
 
-            for (unsigned int k = 0; k < sizeof(ns) / sizeof(*ns); k++) {
+            for (unsigned int k = 0; k < sizeof(ns) / sizeof(*ns); k++)
+            {
                 unsigned int n = ns[k];
                 int *t = malloc(n * sizeof(*t));
 
